273267dfd702357660a467511676548e09689a5e
0xzayd <hamdi.zayd@gmail.com>
2020-11-08 22:06:20 +0100

    added S2Processor class

---
+++ src/S2Processor.py
@@ -0,0 +1,20 @@
+import os
+import zipfile
+import logging
+
+logger = logging.getLogger('S2ProcessorLogger')
+logging.basicConfig(level=logging.INFO)
+
+class S2Processor():
+    def __init__(self):
+        pass
+
+    def get_archive_format(self):
+        pass
+    
+    def unzip(self):
+        pass
+
+    def clip_to_aoi(self):
+        pass
+    
